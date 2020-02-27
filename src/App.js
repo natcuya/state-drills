a02d1dc5d59f318a3b4db477e9e823dbe2464894
Nathaly Cuya <57621005+natcuya@users.noreply.github.com>
2020-02-27 01:14:47 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Bomb from './state-drills/Bomb';
+import HelloWorld from './state-drills/HelloWorld';
+import RouletteGun from './state-drills/RouletteGun';
+//import TheDate from './state/TheDate';
+//import Counter from './state/Counter';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>Hello World</h2>
+        <HelloWorld />
+        <h2>Bomb</h2>
+        <Bomb />
+        <h2>Roulette Gun</h2>
+        <RouletteGun bulletInChamber={3} />
+      </div>
+    );
+  }
 }
 
+
 export default App;
+
